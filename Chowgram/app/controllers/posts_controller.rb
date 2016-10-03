dd7f89f6b4c6abc5c645b5618baf8f24b3c0e54d
haritha <haritha.chowdary916@gmail.com>
2016-10-03 16:51:15 -0500

    day3

--- Chowgram/app/controllers/posts_controller.rb
+++ Chowgram/app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
 
 def index
+ @posts =post.all
+end
 
+def new
+ @post=post.new
+end
+
+def create
+ @post=post.create(post_params)
+ redirect_to posts_path
+end
+
+
+
+private
+
+def post_params
+  params.require(:post).permit(:image. :caption)
 end
 end
